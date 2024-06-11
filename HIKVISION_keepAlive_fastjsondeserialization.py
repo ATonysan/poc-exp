05bc10364fdd4f596f67a43e936865344636ce21
ATonysan <149130294+ATonysan@users.noreply.github.com>
2024-06-11 10:41:55 +0800

    Update HIKVISION_keepAlive_fastjsondeserialization.py

--- HIKVISION_keepAlive_fastjsondeserialization.py
+++ HIKVISION_keepAlive_fastjsondeserialization.py
@@ -18,7 +18,6 @@ def check(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
         "Content-Type": "application/json",
-        "cmd": "echo helloworld"
     }
     
     #Command execution
